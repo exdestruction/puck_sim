3e61aab938c1dab6cbae8a6ca4ecd0ff827f2601
Semyon Kondratev <execute.ink@gmail.com>
2021-08-09 01:41:21 +0200

    Testing Q function

--- model_puck.m
+++ model_puck.m
@@ -5,88 +5,113 @@ sim_steps = round(t_sim / d_t);
 noise = randi([-5, 5], 2, sim_steps);
 noise = noise / 120;
 % noise = randi([0, 0], 2, sim_steps)
+
 % physical parameters
 width_table = 1.5; %[m]
 length_table = 2; %[m]
 m = 0.05; % mass of the puck [kg]
 g = 9.80665;
 f = 0.01034409; % friction coef for aerohockey table
+J = f * g;
 
 %puck geometry
-h = 1;
-r = 5;
-
-% components of motion laws when type D trajectory
+h = 0.02; % m
+r = 0.05; % m
 delta = 2 * f * h / r;
-phi_0 = 0.975 + (0.992 - 0.975) * delta;
-alpha = cos(phi);
-beta = sin(phi);
-p_a = @(phi) real(integral2(@(ro, nu) ...
-	(cos(phi) - sin(phi) .* (ro/r) .* sin(nu)).^2 .* ro ./(pi .* ((sin(phi)).^2 * (ro/r).^2 - 2 .* (cos(phi)) .* (sin(phi)) .* (ro/r) .* sin(nu) + (cos(phi)).^2).^(3/2)),...
-	0, r, 0, 2*pi));
-p_b = @(phi) real(integral2(@(ro, nu) ...
-	((ro/r) .* cos(nu)).^2 .* ro ./ (pi .* ((sin(phi)).^2 * (ro/r).^2 - 2 .* (cos(phi)) .* (sin(phi)) .* (ro/r) .* sin(nu) + (cos(phi)).^2).^(1/2)),...
-	0, r, 0, 2*pi));
-p_a_hat = @(phi) real(integral2(@(ro, nu) ...
-	( (sin(phi) .* (ro/r) - cos(phi) .* sin(nu)).^2) .*(ro/r).^2 .* ro ./ (pi .* ((sin(phi)).^2 * (ro/r).^2 - 2 .* (cos(phi)) .* (sin(phi)) .* (ro/r) .* sin(nu) + (cos(phi)).^2).^(3/2)),...
-	0, r, 0, 2*pi));
-p_c_hat = @(phi) real(integral2(@(ro, nu) ...
-	(sin(phi).^2 .* (ro/r).^2 .* cos(2*nu) + 2 .* cos(phi) .* sin(phi) .* (ro/r) .* sin(nu).^3 - cos(phi).^2 .* sin(nu).^2) .*(ro/r).^2 .* ro ./ (pi .* ((sin(phi)).^2 * (ro/r).^2 - 2 .* (cos(phi)) .* (sin(phi)) .* (ro/r) .* sin(nu) + (cos(phi)).^2).^(3/2)),...
-	0, r, 0, 2*pi));
-p_c =@(phi) p_b(r, phi) - p_a_hat(r, phi);
-
-p_d = @(phi) 1 - delta.^2 .* sin(phi).^2 .* p_b(r, phi) .* p_c(r, phi);
-
-p_parall =@(phi) p_a(phi) / p_d(phi);
-p_omega =@(phi) 2.*(p_a_hat(phi) + (delta.^2 * cos(phi).^2 .* p_a(phi) .* p_b(phi) .* p_c_hat(phi) ./ p_d(phi)));
-p_orth = @(phi) delta .* p_a(phi) .* p_b(phi) ./ p_d(phi);
-J = f * N / m;
 
 
-% r * w / v = tan(phi);
+%Calculating integrals on [0; pi/2] to reduce computing and increase
+%stability
+[p_a_res, p_b_res, p_a_hat_res, p_c_hat_res, p_c_res] = ...
+	calculate_integrals(r);
+
+% functions for calculating integral functions of phi
+p_d = @(phi) (1 - delta.^2 .* sin(phi).^2 .* ...
+	p_b_res(fix(phi/0.01)+1) .* p_c_res(fix(phi/0.01)+1));
+p_parall =@(phi) p_a_res(fix(phi/0.01)+1) ./ p_d_res(fix(phi/0.01)+1);
+p_omega =@(phi) 2.*(p_a_hat_res(fix(phi/0.01)+1) + ...
+	(delta.^2 * cos(phi).^2 .* p_a_res(fix(phi/0.01)+1) ...
+	.* p_b_res(fix(phi/0.01)+1) .* p_c_hat_res(fix(phi/0.01)+1) ...
+	./ p_d_res(fix(phi/0.01)+1)));
+p_orth = @(phi) delta .* p_a_res(fix(phi/0.01)+1) .* p_b_res(fix(phi/0.01)+1) ...
+	./ p_d_res(fix(phi/0.01)+1);
+
+p_d_res = [];
+p_parall_res = [];
+p_omega_res = [];
+p_orth_res = [];
+for phi = 0:0.01:pi/2
+	p_d_res = [p_d_res; p_d(phi)];
+	p_parall_res = [p_parall_res, p_parall(phi)];
+	p_omega_res = [p_omega_res; p_omega(phi)];
+	p_orth_res = [p_orth_res; p_orth(phi)];
+end
+
+
 
 % motion laws
 v_x = Q * cos(phi) * cos(psi);
 v_y = Q * cos(phi) * sin(psi);
-omega = Q * sin(phi) / r;
+w = Q * sin(phi) / r;
 
-Q_dot = -((cos(phi))^2 * p_parall  + (sin(phi))^2 * p_omega)* J;
-phi_dot = sin(phi) * cos(phi) * (p_parall - p_omega) * J / Q;
-psi_dot = -sin(phi) * p_orth * J / Q;
+
+Q_dot =@(phi) -((cos(phi)).^2 .* p_parall_res(fix(phi/0.01)+1) ...
+	+ (sin(phi)).^2 .* p_omega_res(fix(phi/0.01)+1))* J;
+phi_dot =@(phi) sin(phi) .* cos(phi) .* (p_parall(phi) - p_omega(phi)) .* J ./ Q;
+psi_dot =@(phi) -sin(phi) .* p_orth(phi) .* J ./ Q(phi);
 
 
 %Initial conditions, ideal System
 t = 0;
 x_0 = length_table/10; % [m]
 y_0 = width_table/2; % [m]
-v_0 = 1; % m/s
-omega_0 = 10; % rad/s
+
 psi_0 = (pi/180) * (41.4); % [rad] angle to x-axis
+
+ 
+% components of motion laws when type D trajectory
+phi_0 = 0.975 + (0.992 - 0.975) * delta;
+
+v_0 = 1; % m/s
+v_x0 = v_0 * cos(psi_0);
+v_y0 = v_0 * sin(psi_0);
+
+w_0 = tan(phi_0) * v_0 / r; % rad/s
+
+phi_start = atan(r * w_0 / v_0);
+Q_0 = v_0 / cos(phi_start);
+
+%find all Q
+Q = [];
+for step = 1:sim_steps
+	Q = [Q ; integral(Q_dot, 0, step * dt)];
+end
 
 
 X_vec = zeros(2, sim_steps);
-X_vec(:, 1) = [x_0, y_0].';
-A_mat = [1 0; 0 1]; 
-U_vec = [];
-B_mat = [];
+X_vec(:, 1) = [x_0, y_0, v_x0, v_y0].';
+A_mat = [1 0 dt 0; 0 1 0 dt]; 
+% U_vec = [];
+% B_mat = [];
 
 %simulation ideal system trajectory
 for step = 1:sim_steps
-    X_vec(:, step + 1) = A_mat * X_vec(:, step) + B_mat * U_vec;
+%     X_vec(:, step + 1) = A_mat * X_vec(:, step) + B_mat * U_vec;
+	X_vec(:, step + 1) = A_mat * X_vec(:, step);
 	
-	%bounce off the X walls
-	if X_vec(1, step + 1) > length_table || X_vec(1, step + 1) < 0
-		A_mat(1, 3) = A_mat(1,3) * (-1);
-		B_mat(1, 1) = B_mat(1, 1) * (-1);
-		X_vec(:, step + 1) = A_mat * X_vec(:, step) + B_mat * U_vec;
-	end
-	
-	%bounce off the Y walls
-	if X_vec(2, step + 1) > width_table || X_vec(2, step + 1) < 0
-		A_mat(2, 4) = A_mat(2, 4) * (-1);
-		B_mat(1, 2) = B_mat(1, 2) * (-1);
-		X_vec(:, step + 1) = A_mat * X_vec(:, step) + B_mat * U_vec;
-	end
+% % 	bounce off the X walls
+% 	if X_vec(1, step + 1) > length_table || X_vec(1, step + 1) < 0
+% 		A_mat(1, 3) = A_mat(1,3) * (-1);
+% 		B_mat(1, 1) = B_mat(1, 1) * (-1);
+% 		X_vec(:, step + 1) = A_mat * X_vec(:, step) + B_mat * U_vec;
+% 	end
+% 	
+% % 	bounce off the Y walls
+% 	if X_vec(2, step + 1) > width_table || X_vec(2, step + 1) < 0
+% 		A_mat(2, 4) = A_mat(2, 4) * (-1);
+% 		B_mat(1, 2) = B_mat(1, 2) * (-1);
+% 		X_vec(:, step + 1) = A_mat * X_vec(:, step) + B_mat * U_vec;
+% 	end
 	
 	%stop if v < 0
 	if abs(X_vec(3, step + 1)) < 0.0005
@@ -145,4 +170,54 @@ axis([0, length_table, 0, width_table])
 title('Estimated trajectory', 'FontSize', 14)
 xlabel('x, [m]', 'FontSize', 14)
 ylabel('y, [m]', 'FontSize', 14)
+
+
+
+function [p_a_res, p_b_res, p_a_hat_res, p_c_hat_res, p_c_res] = ...
+	calculate_integrals()
+	% calculates values for p_a, p_b, p_a_hat, p_c_hat, p_c 
+	% for phi on [0; pi/2]
+	p_a = @(phi) real(integral2(@(ro, nu) ...
+	(cos(phi) - sin(phi) .* (ro/r) .* sin(nu)).^2 .* ro ./(pi .* ((sin(phi)).^2 * (ro/r).^2 - 2 .* (cos(phi)) .* (sin(phi)) .* (ro/r) .* sin(nu) + (cos(phi)).^2).^(3/2)),...
+	0, r, 0, 2*pi));
+	p_b = @(phi) real(integral2(@(ro, nu) ...
+		((ro/r) .* cos(nu)).^2 .* ro ./ (pi .* ((sin(phi)).^2 * (ro/r).^2 - 2 .* (cos(phi)) .* (sin(phi)) .* (ro/r) .* sin(nu) + (cos(phi)).^2).^(1/2)),...
+		0, r, 0, 2*pi));
+	p_a_hat = @(phi) real(integral2(@(ro, nu) ...
+		( (sin(phi) .* (ro/r) - cos(phi) .* sin(nu)).^2) .*(ro/r).^2 .* ro ./ (pi .* ((sin(phi)).^2 * (ro/r).^2 - 2 .* (cos(phi)) .* (sin(phi)) .* (ro/r) .* sin(nu) + (cos(phi)).^2).^(3/2)),...
+		0, r, 0, 2*pi));
+	p_c_hat = @(phi) real(integral2(@(ro, nu) ...
+		(sin(phi).^2 .* (ro/r).^2 .* cos(2*nu) + 2 .* cos(phi) .* sin(phi) .* (ro/r) .* sin(nu).^3 - cos(phi).^2 .* sin(nu).^2) .*(ro/r).^2 .* ro ./ (pi .* ((sin(phi)).^2 * (ro/r).^2 - 2 .* (cos(phi)) .* (sin(phi)) .* (ro/r) .* sin(nu) + (cos(phi)).^2).^(3/2)),...
+		0, r, 0, 2*pi));
+	p_c =@(phi) p_b(phi) - p_a_hat(phi);
+
+
+	
+	p_a_res = [];
+	p_b_res = [];
+	p_a_hat_res = [];
+	p_c_hat_res = [];
+	p_c_res = [];
+	p_d_res = [];
+	for phi = 0:0.01:pi/2
+		p_a_data = p_a(phi);
+		p_b_data = p_b(phi);
+		p_a_hat_data = p_a_hat(phi);
+		p_c_hat_data = p_c_hat(phi);
+		p_c_data = p_c(phi);
+		p_a_res = [p_a_res; real(p_a_data)];
+		p_b_res = [p_b_res; real(p_b_data)];
+		p_a_hat_res = [p_a_hat_res; real(p_a_hat_data)];
+		p_c_hat_res = [p_c_hat_res; real(p_c_hat_data)];
+		p_c_res = [p_c_res; real(p_c_data)];	
+
+	end
+	p_a_res = clean_data(p_a_res);
+	p_b_res = clean_data(p_b_res);
+	p_a_hat_res = clean_data(p_a_hat_res);
+	p_c_hat_res = clean_data(p_c_hat_res);
+	p_c_res = clean_data(p_c_res);
+end
+
+
 
